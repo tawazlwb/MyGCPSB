3e1e123ef3a12716acb65d8c00ef9d10866140ed
tawazlwb <tawazlwb@gmail.com>
2017-11-14 17:22:39 +0100

    This is myFirst commit

---
+++ src/test/java/com/itis/ikheiry/MyGcpTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.itis.ikheiry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyGcpTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
